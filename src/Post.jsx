fe2a093aeccfd1770e92d02b516427f079935d1c
Diego Alcântara <ddag0309@gmail.com>
2024-01-27 00:35:52 -0300

    Adicionado opcao de curtir e repostar posts

--- src/Post.jsx
+++ src/Post.jsx
@@ -8,43 +8,97 @@ import { FaHeart } from "react-icons/fa6";
 import { FaRegComment } from "react-icons/fa";
 import { FaRetweet } from "react-icons/fa";
 
-import { userInfoDb } from './dbTeste';
+import { postsInfo } from './dbTeste';
 import { mostrarPerfil } from './functions/users';
+import { useState } from 'react';
 
 
 
 function Post(props){
+
+    const [update, setUpdate] = useState(false)
+
+    function alterarLikeRepost(id, btn){
+
+        let index = postsInfo.findIndex(post => post.id == id)
+
+        switch (btn) {
+            case "like":
+                // Aumentar ou diminuir o número de likes baseado se o usuário já curtiou ou não
+                postsInfo[index].curtido? postsInfo[index].likes-- : postsInfo[index].likes++
+
+                // Curtir o post se não tava curtido e vice-versa
+                postsInfo[index].curtido = !postsInfo[index].curtido
+                
+                break;
+
+            case "repost":
+                // Aumentar ou diminuir o número de repost baseado se o usuário já repostou ou não
+                postsInfo[index].repostado? postsInfo[index].reposts-- : postsInfo[index].reposts++
+
+                // Repostar se não tava repostado e vice-versa
+                postsInfo[index].repostado = !postsInfo[index].repostado
+
+                break
+        
+            default:
+                break;
+        }
+
+        
+        // Atualizar o componente
+        setUpdate(!update)
+        
+    }
     
     return (
         <span>
             {props.postsInfo.slice().reverse().map((info) => (
+
+
                 <div className="post postConfigPadrao" key={info.id}>
+
                 <span className='alinhamento'>
                     <BsPersonCircle className='userFoto'></BsPersonCircle>
+
                     <header className="conteudo">
+
                         <span className='linha1'>
+
                         <h3 className='userName'  onClick={() => props.carregarUsuario(info.username)}>{info.username}</h3>
+
                         <p className='data'>{info.data}</p>
                         </span>
                         <p className='texto'>{info.texto}</p>
+
                     </header>
+
                 </span>
 
 
                 <footer>
-                {info.repostado ? (
+                    {/* REPOST BTN */}
+                    <button className={info.repostado? "postRepostado repostBtn" : "repostBtn"} onClick={() => alterarLikeRepost(info.id, "repost")}>
 
-                    <button className='postRepostado'><FaRetweet />{info.reposts}</button>
-                ) : <button><FaRetweet/>{info.reposts}</button>}
+                        <FaRetweet />{info.reposts}
 
+                    </button>
 
-                    
-                    <button><FaRegComment />{info.comentarios}</button>
+                    {/* COMENTARIO BTN */}
+                    <button>
+                        <FaRegComment />{info.comentarios}
+                    </button>
 
-                    {info.curtido ? (
+                    {/* LIKE BTN */}
+                    <button className={info.curtido? "postCurtido likeBtn" : "likeBtn"} id={`likeBtn${info.id}`} onClick={() => alterarLikeRepost(info.id, "like")}>
 
-                        <button className='postCurtido'><FaHeart />{info.likes}</button>
-                    ) : <button><FaRegHeart />{info.likes}</button>}
+                        {info.curtido ? <FaHeart /> : <FaRegHeart />}
+                        {info.likes}
+
+                    </button>
+
+                       
+
                     
                 </footer>
                 </div>
