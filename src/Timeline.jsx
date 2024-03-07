60acf583bb668964c28ebb99c7ca0239b9791800
Diego Alcântara <ddag0309@gmail.com>
2024-03-07 00:23:02 -0300

    style: Adiciona estilo dos modelos de notificacoes

--- src/Timeline.jsx
+++ src/Timeline.jsx
@@ -2,6 +2,10 @@ import "./style/listaEditavel.scss"
 import VoltarTela from "./VoltarTela";
 import { ImNewspaper } from "react-icons/im";
 import { GiTwister } from "react-icons/gi";
+import { BsPersonCircle } from "react-icons/bs";
+import { IoMdNotifications } from "react-icons/io";
+import { MdAddReaction } from "react-icons/md";
+import { MdHealthAndSafety } from "react-icons/md";
 
 
 function Timeline(props){
@@ -15,8 +19,74 @@ function Timeline(props){
             {props.info=="timeline"? (
 
                 <ul className="timelineLista">
-                    <h2>Timeline</h2>
+                    <h2> Notificações
+                    </h2>
+
+                    <li className="itemLista">
+                        <div className="info">
+                            <MdHealthAndSafety className="icone"/>
+                            <div className="principal">
+                                
+                                <h4>Corrigido bug no feed</h4>
+                                <p>01/03/2024</p>
+
+                            </div>
+                        </div>
+
+                        <ImNewspaper className="novidade"/>
+                    </li>
              
+                    <li className="itemLista">
+                        <div className="info">
+                            <MdAddReaction className="icone"/>
+                            <div className="principal">
+                                
+                                <h4>Adicionado notificações no app</h4>
+                                <p>01/03/2024</p>
+
+                            </div>
+                        </div>
+
+                        <ImNewspaper className="novidade"/>
+                    </li>
+
+
+                    <li className="itemLista">
+                        <div className="info">
+                            <BsPersonCircle className="icone"/>
+                            <div className="principal">
+                                
+                                <h4>@user123 criou uma conta no twister</h4>
+                                
+                                <p className="secundario">01/03/2024</p>
+
+                            </div>
+                        </div>
+
+                        <button className="bordaGradient">Abrir</button>
+                    </li>
+
+
+
+                    <li className="itemLista">
+                        <div className="info">
+                            <BsPersonCircle className="icone"/>
+                            <div className="principal">
+                                
+                                <h4>@userteste comentou em seu post</h4>
+                                
+                                <p className="secundario">01/03/2024</p>
+
+                            </div>
+                        </div>
+
+                        <button className="bordaGradient">Abrir</button>
+                    </li>
+
+                    
+
+                    
+
                     <li className="itemLista">
                         <div className="info">
                             <GiTwister className="icone"/>
@@ -26,9 +96,26 @@ function Timeline(props){
                                 <p>01/03/2024</p>
 
                             </div>
+                        </div>
+
+                        <ImNewspaper className="novidade"/>
+                    </li>
+
+                    <li className="itemLista">
+                        <div className="info">
+                            <BsPersonCircle className="icone"/>
+                            <div className="principal">
+                                
+                                <h4>@userteste passou a te seguir</h4>
+                                
+                                <p className="secundario">01/03/2024</p>
+
+                            </div>
 
 
                         </div>
+
+                        <button className="bordaGradient">Abrir</button>
                     </li>
                 </ul>
             ) : null}
